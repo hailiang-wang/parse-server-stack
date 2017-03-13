4cb7359ad50eb921847db4d7f32aaa29bab6a492
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-03-13 23:23:11 +0800

    enable LiveQueryServer

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('./config/environment')
 
 const api = new ParseServer({
   databaseURI: config.app.mongoUri,
-  cloud: __dirname + '/cloud/main.js',
+  cloud: path.join(__dirname, '/cloud/main.js'),
   appId: config.app.appId,
   masterKey: config.app.masterKey, // Add your master key here. Keep it secret!
   serverURL: config.app.serverURL, // Don't forget to change to https if needed
@@ -47,4 +47,34 @@ var port = config.port
 var httpServer = require('http').createServer(app)
 httpServer.listen(port, function () {
   console.log('datacollection running on port ' + port + '.')
+})
+
+httpServer.on('error', function (err) {
+  if (err.syscall !== 'listen') {
+    throw err
+  }
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
+
+  // handle specific listen errors with friendly messages
+  switch (err.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw err
+  }
+})
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer)
+
+process.on('uncaughtException', function (err) {
+  console.error(err)
 })
